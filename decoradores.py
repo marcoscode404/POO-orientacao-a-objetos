c7c4bec62469436d945e48e183e0c116dad13fc1
marcoscode404 <viniciusmg99@outlook.com>
2025-05-24 21:47:21 -0400

    feat: exemplo de decoradores

---
+++ decoradores.py
@@ -0,0 +1,32 @@
+def meu_decorador(func):
+    def wrapper():
+        print("Antes da função ser chamada")
+        func()
+        print("Depois da função ser chamada")
+
+    return wrapper
+
+
+@meu_decorador
+def minha_funcao():
+    print("Minha função foi chamada")
+
+
+minha_funcao()
+
+
+# segunda forma de usar decoradores por meio classes
+class MeuDecoradorDeClasse:
+    def __init__(self, func) -> None:
+        self.func = func
+
+    def __call__(self):
+        print("Antes da funcção ser chamada (decorador de classe)")
+        self.func()
+        print("Depois da função ser chamada (decorador de classe)")
+
+@MeuDecoradorDeClasse
+def segunda_funcao():
+    print("Segunda funcao foi chamada")
+
+segunda_funcao()
